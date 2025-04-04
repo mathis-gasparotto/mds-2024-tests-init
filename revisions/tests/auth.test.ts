74a2ba651135d07a09df531d413b898a2008e1e9
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2025-04-04 12:03:27 +0200

    tests(auth): add check user props

--- revisions/tests/auth.test.ts
+++ revisions/tests/auth.test.ts
@@ -5,6 +5,10 @@ describe("Auth", () => {
   test("should be able to login", () => {
     const user = authenticateUser("john", "secret");
     expect(user).toBeDefined();
+    expect(user).toHaveProperty("id", 1);
+    expect(user).toHaveProperty("username", "john");
+    expect(user).toHaveProperty("email", "john@example.com");
+    expect(user).not.toHaveProperty("password");
   });
 
   test("should be throw an error if the user is not found", () => {
